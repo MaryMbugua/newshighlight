66f1ff43330f500e87233e62999e0b0ad114472b
Mary Mbugua <marymbugua.nm@gmail.com>
2018-01-26 12:25:05 +0300

    put dynamic route for a news article

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     view root page function that returns the index page
     and its data
     '''
-    return render_template('index.html')
+    message = 'Hello World'
+    return render_template('index.html',message = message)
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    '''
+    view news article page function that returns the news article
+    and its data
+    '''
+    return render_template('article.html',id = article_id)
